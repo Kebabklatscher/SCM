feefd3c0e9bba8ee2fa41cde7ec7fa03804f241b
Maaaarkus <102972756+Maaaarkus@users.noreply.github.com>
2024-07-28 15:02:54 +0200

    added readme file

---
+++ README.md
@@ -0,0 +1,12 @@
+This program is used to provide a possible solution for a supply chain logistics problem.
+It was written for a supply chain management course at Hochschule Merseburg.
+Due to limited time, this program is only able to solve orders that are ordered during one single day.
+There are also likely numerous optimization potentials.
+
+original supply chain logistics problem source:
+https://www.sciencedirect.com/science/article/pii/S0360835220303442?via%3Dihub
+
+original dataset source:
+https://brunel.figshare.com/articles/dataset/Supply_Chain_Logistics_Problem_Dataset/7558679
+
+© Markus Heinze, 2024
--- src/main/java/SCM/Main.java
+++ src/main/java/SCM/Main.java
@@ -1,17 +1,3 @@
-/*
-This program is used to provide a possible solution for a supply chain logistics problem.
-It was written for a supply chain management course at Hochschule Merseburg.
-Due to limited time, this program does not fully solve the problem. Some constraints from the original paper were left out, e.g. the shipping time. This program is only able to solve orders that are ordered during one single day.
-There are also likely numerous optimization potentials.
-
-original supply chain logistics problem source:
-https://www.sciencedirect.com/science/article/pii/S0360835220303442?via%3Dihub
-original dataset source:
-https://brunel.figshare.com/articles/dataset/Supply_Chain_Logistics_Problem_Dataset/7558679
-
-© Markus Heinze, 2024
- */
-
 package SCM;
 
 import java.io.IOException;
@@ -31,20 +17,51 @@ public class Main {
         }
 
         int unresolvedOrders = 0;
-        double totalCost = 0;
-        Data outputData = new Data();
-        boolean totalCapacityChanged = false;
+        //calculate all possible routes for every order
         for (Order o: data.orders){
             o.planRoutes(data.products,data.plants,data.freightRates);
         }
-        Plant buffer = null;
+        //apply route choosing algorithm
+        Data outputData = chooseRoutes(data);
+
+        //count unresolved orders
+        for (Order o: outputData.orders){
+            if (o.getPossibleRoutes()==0){
+                unresolvedOrders++;
+            }
+        }
+        System.out.println("Total orders: " + outputData.orders.size());
+        System.out.println("Shipped orders: " + (outputData.orders.size() - unresolvedOrders));
+        System.out.println("Unresolved orders: " + unresolvedOrders);
+
+        //TODO: calculate GROUND shipping cost (combining all routes from the same port on the same day in 1 freightrate)
+        //calculate total cost
+        double totalCost = 0;
+        for (Order o: outputData.orders){
+            if(o.getPossibleRoutes()>0){
+                totalCost+=o.getChosenRoute().getCost();
+            }
+        }
+        //round total cost to cents
+        totalCost = (double) Math.round(totalCost * 100) /100;
+        System.out.println("Total cost: " + totalCost + " €");
+        //write all orders to Excel file
+        outputData.writeFile();
+        long timeEnd = System.currentTimeMillis();
+
+        System.out.println("Total execution time: " + (timeEnd-timeStart) + "ms");
+    }
+
+    public static Data chooseRoutes(Data data){
+        Data outputData = new Data();
+        boolean totalCapacityChanged = false;
+        Plant buffer;
         while (!data.orders.isEmpty()){
             for (Order o: data.orders) {
-                //calculate, how many routes are possible for every order
+                //check if routes are now impossible due to capacity
                 o.checkRoutes();
                 switch (o.getPossibleRoutes()){
                     case 0:
-                        unresolvedOrders++;
                         outputData.orders.add(o);
                         break;
                     case 1:
@@ -67,27 +84,11 @@ public class Main {
                         }
                         break;
                 }
-                //TODO: add optimization of combining orders that use the same carrier (add weight -> reduce cost)
             }
             data.orders.removeAll(outputData.orders);
             totalCapacityChanged = false;
         }
-
-        System.out.println(unresolvedOrders + " Orders out of " + outputData.orders.size() + " could not be shipped");
-        //calculate total cost
-        for (Order o: outputData.orders){
-            if(o.getPossibleRoutes()>0){
-                totalCost+=o.getChosenRoute().getCost();
-            }
-        }
-        //round total cost to cents
-        totalCost = (double) Math.round(totalCost * 100) /100;
-        System.out.println("Total cost: " + totalCost + " €");
-        //write all orders to excel file
-        outputData.writeFile();
-        long timeEnd = System.currentTimeMillis();
-
-        System.out.println("Total execution time: " + (timeEnd-timeStart) + "ms");
+        return outputData;
     }
 }
 
